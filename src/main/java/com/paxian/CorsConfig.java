6e1db41b9e63f93aec1f17631c5eb1ad30915168
Pius <ppax1706@gmail.com>
2021-05-11 02:17:35 +0100

    initial commit

--- src/main/java/com/paxian/CorsConfig.java
+++ src/main/java/com/paxian/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200", "https://admin-controller-with-angular.herokuapp.com")
+                        .allowedOrigins(allowedOrigin)
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
